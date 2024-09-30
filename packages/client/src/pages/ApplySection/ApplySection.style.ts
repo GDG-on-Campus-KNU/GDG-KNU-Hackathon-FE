d2e130bf6a03af44909bffd0d279c93d26e35671
toothlessdev <toothless042@gmail.com>
2024-10-01 03:43:43 +0900

    feat: ApplySection 구현

--- packages/client/src/pages/ApplySection/ApplySection.style.ts
+++ packages/client/src/pages/ApplySection/ApplySection.style.ts
@@ -1,1 +1,118 @@
+import { BREAKPOINT } from "@/styles/breakpoint";
+
 import styled from "@emotion/styled";
+
+export const EnvelopWrapper = styled.div`
+    position: relative;
+    top: 0;
+
+    width: 100%;
+    height: 1000px;
+
+    margin: 0px auto;
+    margin-top: 200px;
+    padding: 0px 40px;
+
+    ${BREAKPOINT.MOBILE} {
+        padding: 0px 20px;
+    }
+`;
+
+export const EnvelopContainer = styled.div`
+    position: relative;
+    z-index: 1;
+
+    width: 100%;
+    max-width: 640px;
+
+    margin: 0px auto;
+    border-radius: 16px;
+    padding-bottom: 180px;
+
+    background-color: #fff;
+
+    ${BREAKPOINT.MOBILE} {
+        padding-bottom: 150px;
+    }
+`;
+
+export const EnvelopBack = styled.img`
+    z-index: 0;
+    position: absolute;
+    bottom: 200px;
+    display: block;
+
+    width: 100%;
+    max-width: 680px;
+
+    margin: 0px auto;
+`;
+
+export const EnvelopFront = styled.img`
+    z-index: 1;
+    position: absolute;
+    bottom: 200px;
+    display: block;
+
+    width: 100%;
+    max-width: 680px;
+    margin: 0px auto;
+`;
+
+export const Form = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+
+    margin: 0px auto;
+    padding: 10px;
+`;
+
+export const Logo = styled.img`
+    width: 80px;
+
+    margin: 0px auto;
+    margin-top: 20px;
+    margin-bottom: 40px;
+`;
+
+export const Line = styled.hr`
+    width: 100%;
+    border: 1px dashed #c8c8c8;
+`;
+
+export const FormContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+
+    width: 100%;
+
+    padding: 10px;
+    border-radius: 16px;
+
+    background-color: #fff;
+
+    & > div {
+        flex: 1;
+    }
+    input {
+        margin: 14px 0px;
+        flex-grow: 1;
+        flex-shrink: 0;
+    }
+    label {
+        width: 100px;
+        span {
+            margin-right: 10px;
+            margin-left: auto;
+        }
+    }
+`;
+
+export const ApplyLabel = styled.label`
+    width: 100%;
+
+    display: flex;
+    align-items: center;
+`;
--- packages/client/src/pages/ApplySection/ApplySection.tsx
+++ packages/client/src/pages/ApplySection/ApplySection.tsx
@@ -1,5 +1,100 @@
+import gsap from "gsap";
+
+import { ApplyPositionContainer } from "@/components/ApplyPosition/Container";
+import { ApplyPositionItem } from "@/components/ApplyPosition/Item";
+import { SubmitButton } from "@/components/Button/SubmitButton";
+import { Input } from "@/components/Form/Input";
+import { Text } from "@/components/Text/Text";
+import { SectionTitle } from "@/components/Title/SectionTitle";
+
+import { useApply } from "@/hooks/useApply";
+import { useApplySectionAnimation } from "@/hooks/useApplySectionAnimation";
+
+import envelopFront from "@/assets/images/envelop-front.svg";
+import logo from "@/assets/images/logoBlue.svg";
+
 import * as ApplySectionStyles from "./ApplySection.style";
+import { ApplyPositionContextProvider } from "@/context/ApplyPositionContext";
 
 export const ApplySection = () => {
-    return <></>;
+    useApplySectionAnimation();
+
+    const {
+        nameRef,
+        studentIdRef,
+        majorRef,
+        phoneNumberRef,
+        githubIdRef,
+        teamNameRef,
+        emailRef,
+        position,
+        setPosition,
+        handleSubmit,
+    } = useApply();
+
+    return (
+        <>
+            <SectionTitle
+                subtitle="APPLY"
+                title={
+                    <>
+                        <span>미래를 향한 도전 속으로, </span>
+                        <span>여러분을 초대합니다!</span>
+                    </>
+                }
+            />
+
+            <ApplySectionStyles.EnvelopWrapper id="apply-section">
+                <ApplySectionStyles.EnvelopContainer className="envelop-container">
+                    <ApplySectionStyles.Form>
+                        <ApplySectionStyles.Logo src={logo} />
+
+                        <ApplySectionStyles.FormContainer>
+                            <Input ref={nameRef} label="이름" type="text" />
+                            <Input ref={studentIdRef} label="학번" type="number" />
+                            <Input ref={majorRef} label="전공" type="text" />
+                            <Input ref={phoneNumberRef} label="전화번호" type="text" />
+                            <Input ref={emailRef} label="이메일" type="email" />
+                        </ApplySectionStyles.FormContainer>
+
+                        <ApplySectionStyles.Line />
+
+                        <ApplySectionStyles.FormContainer>
+                            <ApplySectionStyles.ApplyLabel>
+                                <Text size="s" color="var(--color-blue)" weight="bold">
+                                    직렬 선택
+                                </Text>
+                            </ApplySectionStyles.ApplyLabel>
+
+                            <ApplyPositionContextProvider onChange={setPosition}>
+                                <ApplyPositionContainer>
+                                    <ApplyPositionItem value="FRONTEND">프론트엔드</ApplyPositionItem>
+                                    <ApplyPositionItem value="BACKEND">백엔드</ApplyPositionItem>
+                                    <ApplyPositionItem value="ANDROID">안드로이드</ApplyPositionItem>
+                                    <ApplyPositionItem value="DESIGN">디자인</ApplyPositionItem>
+                                    <ApplyPositionItem value="PM">기획</ApplyPositionItem>
+                                </ApplyPositionContainer>
+                            </ApplyPositionContextProvider>
+                        </ApplySectionStyles.FormContainer>
+
+                        <ApplySectionStyles.Line />
+
+                        <ApplySectionStyles.FormContainer>
+                            <Input
+                                ref={githubIdRef}
+                                label="깃허브 아이디"
+                                type="text"
+                                disabled={position === "DESIGN"}
+                            />
+                            <Input ref={teamNameRef} label="팀이름" type="text" />
+                        </ApplySectionStyles.FormContainer>
+
+                        <SubmitButton onClick={handleSubmit}>신청하기</SubmitButton>
+                    </ApplySectionStyles.Form>
+                </ApplySectionStyles.EnvelopContainer>
+
+                <ApplySectionStyles.EnvelopFront className="envelop" src={envelopFront} />
+            </ApplySectionStyles.EnvelopWrapper>
+        </>
+    );
 };
