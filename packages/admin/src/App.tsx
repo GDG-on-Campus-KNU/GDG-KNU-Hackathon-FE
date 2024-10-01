7011ae2830cb1f3a8f7fcce858fedc059a93a153
Dobbymin <gmin0701@knu.ac.kr>
2024-10-02 01:29:03 +0900

    feat: global style 적용

--- packages/admin/src/App.tsx
+++ packages/admin/src/App.tsx
@@ -1,10 +1,13 @@
 import { queryClient } from "./api/instance";
 import { Routes } from "./routes";
+import { globalStyle } from "./theme";
+import { Global } from "@emotion/react";
 import { QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
     return (
         <QueryClientProvider client={queryClient}>
+            <Global styles={globalStyle} />
             <Routes />
         </QueryClientProvider>
     );
