80292f8fc716630bee2208f133edeeb72146cdc5
Dobbymin <gmin0701@knu.ac.kr>
2024-10-02 01:28:42 +0900

    feat: css 파일 경로 추가

--- packages/admin/src/main.tsx
+++ packages/admin/src/main.tsx
@@ -3,5 +3,6 @@ import { createRoot } from "react-dom/client";
 import "@/styles/reset.css";
 
 import App from "./App.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
