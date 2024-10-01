4c24f44392eec7af905309160050961811c07055
toothlessdev <toothless042@gmail.com>
2024-10-01 17:58:13 +0900

    feat: ApproveButton 컴포넌트 구현

---
+++ packages/client/src/components/Button/ApproveButton.style.ts
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+export const Element = styled.button`
+    display: flex;
+
+    margin: 0px auto;
+    border-radius: 20px;
+    padding: 10px 20px;
+
+    color: #9fa0a0;
+
+    transition:
+        background-color 0.5s ease-in-out,
+        color 0.5s ease-in-out;
+
+    &:hover {
+        background-color: var(--color-blue);
+        color: #fff;
+    }
+`;
