c7768e5fce3348bb65278d83a28b4da517039893
toothlessdev <toothless042@gmail.com>
2024-10-01 14:48:49 +0900

    feat: 타임라인 섹션 스켈레톤 코드 작성

--- packages/client/src/pages/TimeLineSection/TimeLineSection.tsx
+++ packages/client/src/pages/TimeLineSection/TimeLineSection.tsx
@@ -1,3 +1,17 @@
+import { SectionTitle } from "@/components/Title/SectionTitle";
+
 export const TimeLineSection = () => {
-    return <></>;
+    return (
+        <>
+            <SectionTitle
+                subtitle="TIMELINE"
+                title={
+                    <>
+                        <span>VISION CHALLENGETHON의</span> <br />
+                        <span>타임라인을 알려드립니다</span>
+                    </>
+                }
+            />
+        </>
+    );
 };
