0d840162bdb6e017babba0e690cb1712423286cf
toothlessdev <toothless042@gmail.com>
2024-10-05 14:32:08 +0900

    chore: font-face woff format 적용

--- packages/client/src/styles/fonts.ts
+++ packages/client/src/styles/fonts.ts
@@ -3,22 +3,22 @@ import { css } from "@emotion/react";
 export const fontStyles = css`
     @font-face {
         font-family: AvantGrade sans-serif;
-        src: url("@/assets/fonts/ITC Avant Garde Gothic Std Bold.woff");
+        src: url("@/assets/fonts/ITC Avant Garde Gothic Std Bold.woff") format("woff");
         font-weight: bolder;
     }
     @font-face {
         font-family: AvantGrade sans-serif;
-        src: url("@/assets/fonts/ITC Avant Garde Gothic Std Demi.woff");
+        src: url("@/assets/fonts/ITC Avant Garde Gothic Std Demi.woff") format("woff");
         font-weight: bold;
     }
     @font-face {
         font-family: AvantGrade sans-serif;
-        src: url("@/assets/fonts/ITC Avant Garde Gothic Std Book.woff");
+        src: url("@/assets/fonts/ITC Avant Garde Gothic Std Book.woff") format("woff");
         font-weight: normal;
     }
     @font-face {
         font-family: AvantGrade sans-serif;
-        src: url("@/assets/fonts/ITC Avant Garde Gothic Std Extra Light.woff");
+        src: url("@/assets/fonts/ITC Avant Garde Gothic Std Extra Light.woff") format("woff");
         font-weight: lighter;
     }
 `;
