1c2525baae7e7b10c37ca025472538b5312e457c
toothlessdev <toothless042@gmail.com>
2024-10-05 19:41:07 +0900

    fix: 모바일에서 지원버튼 안눌리는 현상 해결 및 SectionTitle 반응형 폰트크기 조정

--- packages/client/src/components/Title/SectionTitle.style.ts
+++ packages/client/src/components/Title/SectionTitle.style.ts
@@ -13,11 +13,32 @@ export const SubTitle = styled.h3`
     text-align: center;
     margin: 34px 0px;
 
+    font-size: 18px;
+    font-weight: lighter;
+
+    color: #fff;
+
     ${BREAKPOINT.MOBILE} {
         margin: 32px 0px;
+        font-size: 10px;
+    }
+    ${BREAKPOINT.TABLET} {
+        font-size: 15px;
     }
 `;
 
 export const Title = styled.h2`
     text-align: center;
+
+    font-size: 37px;
+    font-weight: bold;
+
+    color: #fff;
+
+    ${BREAKPOINT.MOBILE} {
+        font-size: 15px;
+    }
+    ${BREAKPOINT.TABLET} {
+        font-size: 30px;
+    }
 `;
