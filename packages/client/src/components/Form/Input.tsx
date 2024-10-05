7b14c646b3079a2f972034bd087d5c3b4919d0e2
toothlessdev <toothless042@gmail.com>
2024-10-05 14:41:21 +0900

    fix: forwardRef 제네릭 타입 오류 해결

--- packages/client/src/components/Form/Input.tsx
+++ packages/client/src/components/Form/Input.tsx
@@ -7,7 +7,7 @@ export interface InputProps extends React.ComponentProps<"input"> {
     label?: string;
 }
 
-export const Input = forwardRef(({ label, ...rest }: InputProps, ref) => {
+export const Input = forwardRef<HTMLInputElement>(({ label, ...rest }: InputProps, ref) => {
     return (
         <InputStyles.Container>
             <InputStyles.InputLabel htmlFor={label}>
