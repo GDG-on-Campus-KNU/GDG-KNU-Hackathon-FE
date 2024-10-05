e552aacee958fa5794445e1e4a0ebfbd78734150
toothlessdev <toothless042@gmail.com>
2024-10-05 20:39:57 +0900

    feat: 빌드타임 사전렌더링 적용

--- packages/client/src/pages/Page.tsx
+++ packages/client/src/pages/Page.tsx
@@ -1,3 +1,6 @@
+import { fontStyles } from "@/styles/fonts";
+import { globalStyle } from "@/styles/global";
+
 import { NavBar } from "../layouts/NavBar/NavBar";
 import { ApplySection } from "./ApplySection/ApplySection";
 import { HeroSection } from "./HeroSection/HeroSection";
@@ -7,22 +10,27 @@ import { SponsorSection } from "./SponsorSection/SponsorSection";
 import { TimeLineSection } from "./TimeLineSection/TimeLineSection";
 import data from "@/__data__/index.json";
 import { ApplyModalContextProvider } from "@/context/ModalContext";
+import { Global } from "@emotion/react";
 
 export default function Page() {
     return (
-        <ApplyModalContextProvider>
-            <PageStyles.Main>
-                <NavBar location={data.navTop.location} locationDetail={data.navTop.locationDetail} />
-                <HeroSection
-                    title={data.heroSection.title}
-                    date={data.heroSection.date}
-                    location={data.heroSection.location}
-                />
-                <IntroductionSection />
-                <TimeLineSection />
-                <SponsorSection imgLists={data.sponsorSection.imgLists} />
-                <ApplySection />
-            </PageStyles.Main>
-        </ApplyModalContextProvider>
+        <>
+            <ApplyModalContextProvider>
+                <PageStyles.Main>
+                    <NavBar location={data.navTop.location} locationDetail={data.navTop.locationDetail} />
+                    <HeroSection
+                        title={data.heroSection.title}
+                        date={data.heroSection.date}
+                        location={data.heroSection.location}
+                    />
+                    <IntroductionSection />
+                    <TimeLineSection />
+                    <SponsorSection imgLists={data.sponsorSection.imgLists} />
+                    <ApplySection />
+                </PageStyles.Main>
+            </ApplyModalContextProvider>
+
+            <Global styles={[globalStyle, fontStyles]} />
+        </>
     );
 }
