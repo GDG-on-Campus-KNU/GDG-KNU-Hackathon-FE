0b9fa895bc9323fd2177d42a510994a98f2d13d0
Dobbymin <gmin990624@gmail.com>
2024-11-10 01:12:34 +0900

    fix: 삭제 기능 추가

---
+++ packages/admin/src/features/people-management/data/table-header/TableHeader.ts
@@ -0,0 +1,10 @@
+export const TABLE_HEADERS = [
+    { label: "이름", key: "name" },
+    { label: "학번", key: "studentId" },
+    { label: "전화번호", key: "phoneNumber" },
+    { label: "팀 이름", key: "teamName" },
+    { label: "전공", key: "major" },
+    { label: "입금 확인", key: "accepted" },
+    { label: "체크인 상태", key: "checkined" },
+    { label: "삭제", key: "delete" },
+];
---
+++ packages/admin/src/features/people-management/data/table-header/index.ts
@@ -0,0 +1,1 @@
+export { TABLE_HEADERS } from "./TableHeader";
